ec506d8384435a2e6667b448acb5bd8d1bfb7af4
ErikDanielsson <danielsson.erik.0@gmail.com>
2025-04-30 12:51:47 +0200

    Add tiny subroot branch to trees

--- bin/generate_trees.R
+++ bin/generate_trees.R
@@ -17,9 +17,9 @@ set.seed(genid)
 tree <- rcoal(ntips_symbiont, rooted = TRUE)
 tree$tip.label <- paste0("Symbiont", 1:ntips_symbiont)
 
-height <- node.depth.edgelength(tree)[1]
-scaling_factor <- 2.0 / height
-tree$edge.length <- tree$edge.length * scaling_factor
+sim_height <- node.depth.edgelength(tree)[1]
+height <- 10.0 * log(ntips_symbiont)
+tree$edge.length <- tree$edge.length * height / sim_height
 
 is.binary(tree)
 is.ultrametric(tree)
@@ -29,16 +29,17 @@ is.rooted(tree)
 host_tree <- rcoal(ntips_host, rooted = TRUE)
 host_tree$tip.label <- paste0("Host", 1:ntips_host)
 
+# Write tree to temp file
+tmp_symbiont_tree_fn <- paste0("tmp", genid, ".tre")
+write.tree(tree, tmp_symbiont_tree_fn)
+
+# Add tiny subroot branch to tree
+subroot_length <- height * 1e-12
+tree_string <- readLines(tmp_symbiont_tree_fn)
+tree_tiny_stem_string <- paste0(
+    sub(");$", "):", tree_string), subroot_length, ";"
+)
 host_tree_fn <- paste0("host_tree.", genid, ".tre")
 symbiont_tree_fn <- paste0("symbiont_tree.", genid, ".tre")
 write.tree(host_tree, host_tree_fn)
-write.tree(tree, symbiont_tree_fn)
-
-# Add subroot branch to tree
-tree_string <- readLines(symbiont_tree_fn)
-tree_tiny_stem_string <- sub(");$", "):0.01;", tree_string)
-tiny_tree_fn <- paste0("tiny_stem_tree.", genid, ".tre")
-writeLines(tree_tiny_stem_string, tiny_tree_fn)
-tree_long_stem_string <- sub(");$", "):2.0;", tree_string)
-long_tree_fn <- paste0("long_stem_tree.", genid, ".tre")
-writeLines(tree_long_stem_string, long_tree_fn)
+writeLines(tree_tiny_stem_string, symbiont_tree_fn)
